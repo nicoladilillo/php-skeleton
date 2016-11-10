151ffdae34feca95f2c697e5f0d8d646398d0606
Nicola Dilillo <nicoladilillo98@gmail.com>
2016-11-11 00:42:25 +0100

    First page responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 (function() {
-  var x;
-  x = document.getElementsByClassName("results")[0];
-  var y = document.getElementsByTagName("body")[0];
-  if(x) {
-    y.style.backgroundImage = 'linear-gradient(-78deg, #47D1AF 55%, transparent 0%), url("/assets/images/recycle.png")';
-  }
-  else {
-    y.style.backgroundImage = 'linear-gradient(78deg, #47D1AF 55%, transparent 0%), url("/assets/images/recycle.png")';
+  if(window.screen.availWidth > 500) {
+    var x;
+    x = document.getElementsByClassName("results")[0];
+    var y = document.getElementsByTagName("body")[0];
+    if(x) {
+      y.style.backgroundImage = 'linear-gradient(-78deg, #47D1AF 55%, transparent 0%), url("/assets/images/recycle.png")';
+    }
+    else {
+      y.style.backgroundImage = 'linear-gradient(78deg, #47D1AF 55%, transparent 0%), url("/assets/images/recycle.png")';
+    }
   }
 })();
